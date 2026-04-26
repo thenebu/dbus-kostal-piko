b0a5ece9d29600a31fac5ea92b518779e865d788
thenebu <uwe@thenebu.de>
2026-04-26 19:06:27 +0200

    Add thenebu branding to firmware version string

--- dbus-kostal-piko.py
+++ dbus-kostal-piko.py
@@ -87,7 +87,7 @@ class KostalPikoService:
 
         # Management paths
         self._dbusservice.add_path("/Mgmt/ProcessName", __file__)
-        self._dbusservice.add_path("/Mgmt/ProcessVersion", "1.0.0")
+        self._dbusservice.add_path("/Mgmt/ProcessVersion", "1.0.0-thenebu")
         self._dbusservice.add_path("/Mgmt/Connection", f"Modbus RTU {SERIAL_PORT} @{SLAVE_ADDR}")
 
         # Mandatory paths
@@ -96,7 +96,7 @@ class KostalPikoService:
         self._dbusservice.add_path("/ProductName", device_name)
         self._dbusservice.add_path("/CustomName", device_name)
         self._dbusservice.add_path("/Serial", serial_number)
-        self._dbusservice.add_path("/FirmwareVersion", "1.0.0")
+        self._dbusservice.add_path("/FirmwareVersion", "1.0.0-thenebu")
         self._dbusservice.add_path("/Connected", 1)
         self._dbusservice.add_path("/Latency", None)
         self._dbusservice.add_path("/ErrorCode", 0)
